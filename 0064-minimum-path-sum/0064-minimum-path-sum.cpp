d98e81922d04a74dcb9f1a1d0e610703cccd8d19
Mittal0604 <prasoonmittal424@gmail.com>
2026-06-30 18:29:18 +0530

    Time: 0 ms (100%), Space: 16.3 MB (60.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+//     int n,m;
+//     vector<vector<int>> dp;
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         n = grid.size(), m = grid[0].size();
+//         dp.resize(n,vector<int>(m,-1));
+//         return find(0,0,grid);
+//     }
+//     int find(int i,int j,vector<vector<int>>& grid) {
+//         if (i == n || j == m) return 1e9;
+//         if (i == n-1 && j == m-1) return grid[i][j];
+//         if (dp[i][j] != -1) return dp[i][j];
+//         int right = find(i,j+1,grid);
+//         int down = find(i+1,j,grid);
+//         return dp[i][j] = grid[i][j] + min(right,down);
+//     }
+// };
+
+// 
 class Solution {
-    int n,m;
-    vector<vector<int>> dp;
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        n = grid.size(), m = grid[0].size();
-        dp.resize(n,vector<int>(m,-1));
-        return find(0,0,grid);
-    }
-    int find(int i,int j,vector<vector<int>>& grid) {
-        if (i == n || j == m) return 1e9;
-        if (i == n-1 && j == m-1) return grid[i][j];
-        if (dp[i][j] != -1) return dp[i][j];
-        int right = find(i,j+1,grid);
-        int down = find(i+1,j,grid);
-        return dp[i][j] = grid[i][j] + min(right,down);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0] = grid[0][0];
+        for (int j = 1;j<m;j++) dp[0][j] = dp[0][j-1] + grid[0][j];
+        for (int i = 1;i<n;i++) dp[i][0] = dp[i-1][0] + grid[i][0];
+        for (int i = 1;i<n;i++) {
+            for (int j = 1;j<m;j++) {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
