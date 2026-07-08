0532582596bae849426b461cf347c626c2c35eb4
Mittal0604 <prasoonmittal424@gmail.com>
2026-07-08 23:02:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (72.77%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if(dividend==divisor) return 1;
-        bool sign=true;
-        if(dividend>=0 && divisor<0) sign=false;
-        else if(dividend<0 && divisor>0) sign=false;
+        bool sgn=true;
+        if(dividend>=0 && divisor<0) sgn=false;
+        else if(dividend<0 && divisor>0) sgn=false;
         long n=abs((long)dividend);
         long d=abs((long)divisor);
         long quotient=0;
@@ -16,10 +16,10 @@ public:
             quotient+=1<<cnt;
             n-=(d<<cnt);
         }
-        if(quotient==(1<<31) && sign) return INT_MAX;
-        if(quotient==(1<<31) && !sign) return INT_MIN;
+        if(quotient==(1<<31) && sgn) return INT_MAX;
+        if(quotient==(1<<31) && !sgn) return INT_MIN;
 
-        return sign ? quotient : -quotient;
+        return sgn ? quotient : -quotient;
         
     }
 };
