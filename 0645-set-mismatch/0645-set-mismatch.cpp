4250300b122f4ba3e7d5f1cb8e2e81e21afc23b1
Mittal0604 <prasoonmittal424@gmail.com>
2026-09-16 14:48:51 +0530

    Time: 27 ms (26.96%), Space: 34.8 MB (6.59%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int>ans;
-        map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
+        unordered_map<int,int> freq;
+        vector<int> ans;
+        for( int x : nums){
+            freq[x]++;
         }
-        int duplicate;
-        int missing;
-        for(int i=1;i<=nums.size();i++){
-            if(mpp[i]==2){
-                duplicate=i;
-            }
-            else if(mpp[i]==0){
-                missing=i;
+        for(auto it : freq){
+            if(it.second == 2){
+                ans.push_back(it.first);
+            }  
+        }
+        for(int i = 1; i <= nums.size(); i++){
+            if(freq.find(i) == freq.end()) {
+                ans.push_back(i);
             }
         }
-        ans.push_back(duplicate);
-        ans.push_back(missing);   
         return ans;
-        }  
+    }
 };
