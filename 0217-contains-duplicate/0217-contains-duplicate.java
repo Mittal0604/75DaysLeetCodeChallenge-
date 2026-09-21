f6f3e6ea26f940af65c270a524bc6d82ae8496c3
Mittal0604 <prasoonmittal424@gmail.com>
2026-09-21 17:55:32 +0530

    Time: 24 ms (28.32%), Space: 81.7 MB (81.99%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int c = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i+1]) c++;
+        } 
+        if(c >= 1) return true;
+        return false;
+    }
+}
